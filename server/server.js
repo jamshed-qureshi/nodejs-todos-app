12e8158432c0488ef4b28ec2eaea7ab03619d0fe
jamshed-qureshi <jamshed.alam_qureshi@yahoo.com>
2018-05-14 14:54:05 +0530

    Added test cases for route /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var {ObjectId} = require('mongodb');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require("./db/mongoose");
 var {Todo} = require("./models/todo");
@@ -32,7 +32,7 @@ app.get("/todos", (req, res) => {
 
 app.get("/todos/:id", (req, res) => {
 	var id = req.params.id;
-	if(!ObjectId.isValid) {
+	if(!ObjectID.isValid) {
 		return res.status(404).send();
 	}
 
