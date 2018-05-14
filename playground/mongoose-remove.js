994a3e0e94953baadefa13d4be1cfdedb4bad3d5
jamshed-qureshi <jamshed.alam_qureshi@yahoo.com>
2018-05-14 16:00:45 +0530

    Add delete todos by Id

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,20 @@
+const {ObjectId} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+// 	console.log(result);
+// });
+
+// Todo.findOneAndRemove
+// Todo.findByIdAndRemove
+
+// Todo.findOneAndRemove({ _id: '5af95e634723674614a088e0'}).then((todo) => {
+// 	console.log(todo);
+// });
+
+// Todo.findByIdAndRemove('5af95e634723674614a088e0').then((todo) => {
+// 	console.log(todo);
+// });
