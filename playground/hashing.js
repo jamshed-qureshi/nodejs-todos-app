ff62ce069a28172e6455b31be348119f036ac9bb
jamshed-qureshi <jamshed.alam_qureshi@yahoo.com>
2018-05-15 17:07:01 +0530

    Hash user password before save

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,11 +1,15 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-var data = {
-	id: 10
-};
+var password = "abc123";
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
 
-var decoded = jwt.verify(token, '123abc');
-console.log("decoded", decoded);
+// var data = {
+// 	id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log("decoded", decoded);
