ca00f175312b77754e92b2f245bd6080528e6daf
jamshed-qureshi <jamshed.alam_qureshi@yahoo.com>
2018-05-15 10:32:44 +0530

    Setup config file & env variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,4 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
-
-module.exports = {mongoose};
+mongoose.connect(process.env.MONGODB_URI)
